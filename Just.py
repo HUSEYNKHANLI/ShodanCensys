4236ff6de8d5d5464d3039bce1b80c1dae375006
HUSEYNKHANLI <67484695+HUSEYNKHANLI@users.noreply.github.com>
2023-09-25 10:24:07 +0400

    Update Just.py

--- Just.py
+++ Just.py
@@ -1,11 +1,18 @@
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
-URL = 'https://www.gartner.com/reviews/market/web-content-management'
-response = requests.get(URL)
-soup = BeautifulSoup(response.content, 'html.parser')
+# Path to the ChromeDriver executable (adjust as necessary)
+CHROMEDRIVER_PATH = '/path/to/chromedriver'
 
-# Trying a more general approach by just looking for img tags with alt attributes.
+# Set up the Selenium driver
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH)
+driver.get('https://www.gartner.com/reviews/market/web-content-management')
+
+# Give the page some time to load dynamically-loaded content
+driver.implicitly_wait(10)
+
+# Get the page source and parse with Beautiful Soup
+soup = BeautifulSoup(driver.page_source, 'html.parser')
 product_names = [img['alt'] for img in soup.find_all('img') if img.has_attr('alt') and img['alt']]
 
 # Save the product names to a .txt file
@@ -13,4 +20,5 @@ with open('products.txt', 'w') as file:
     for product in product_names:
         file.write(product + '\n')
 
+driver.quit()
 print(product_names)
