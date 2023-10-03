45056c5522a2e720b58bb811fee499a484d0235e
HUSEYNKHANLI <67484695+HUSEYNKHANLI@users.noreply.github.com>
2023-10-03 11:48:48 +0400

    Update Just.py

--- Just.py
+++ Just.py
@@ -8,7 +8,7 @@ def get_urls_from_file(keyword, filename="urls.txt"):
             key, urls = line.strip().split(": ")
             if key == keyword:
                 return urls.split(", ")
-    return None, None, None
+    return None, None, None, None
 
 def scrape_trustradius_product_names(url):
     response = requests.get(url)
@@ -43,8 +43,21 @@ def scrape_capterra_product_names(url):
     matches = re.findall(pattern, content)
     return matches
 
+def scrape_github_product_names(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Failed to retrieve GitHub webpage. Status code: {response.status_code}")
+        return []
+
+    content = response.text
+    pattern = r'style="max-height:275px" href="(/[^/]+/[^/]+)" data-view-component="true"'
+    matches = re.findall(pattern, content)
+    # Extracting only the repository name from the matches
+    repo_names = [match.split('/')[-1] for match in matches]
+    return repo_names
+
 def main(keyword):
-    trustradius_url, gartner_url, capterra_url = get_urls_from_file(keyword)
+    trustradius_url, gartner_url, capterra_url, github_url = get_urls_from_file(keyword)
     
     if not trustradius_url:
         print(f"No TrustRadius URL found for keyword: {keyword}")
@@ -71,6 +84,15 @@ def main(keyword):
         # Scrape Capterra
         capterra_products = scrape_capterra_product_names(capterra_url)
         for name in capterra_products:
+            print(name)
+
+    if not github_url:
+        print(f"No GitHub URL found for keyword: {keyword}")
+    else:
+        print(f"\nGitHub Repositories for {keyword}:")
+        # Scrape GitHub
+        github_repos = scrape_github_product_names(github_url)
+        for name in github_repos:
             print(name)
 
 if __name__ == "__main__":
