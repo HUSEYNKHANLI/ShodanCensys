43c93f69cf910ae7e525bd9b588d9ff381d9e00b
HUSEYNKHANLI <67484695+HUSEYNKHANLI@users.noreply.github.com>
2023-10-06 15:00:51 +0400

    Update sss.py

--- sss.py
+++ sss.py
@@ -1,11 +1,10 @@
-import aiohttp
-import asyncio
-import csv
+import requests
 import re
 import sys
 import logging
 import pickle
 import os
+import csv
 
 # Configuration
 FILENAME = "urls.txt"
@@ -31,15 +30,14 @@ def save_to_cache(keyword, data):
     with open(cache_file, 'wb') as f:
         pickle.dump(data, f)
 
-async def make_request_async(url):
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(url) as response:
-                response.raise_for_status()
-                return await response.text()
-        except aiohttp.ClientError as e:
-            logging.error(f"Failed to retrieve webpage {url}. Error: {e}")
-            return None
+def make_request(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text
+    except requests.RequestException as e:
+        logging.error(f"Failed to retrieve webpage {url}. Error: {e}")
+        return None
 
 def get_urls_from_file(keyword, filename=FILENAME):
     with open(filename, 'r') as file:
@@ -49,47 +47,41 @@ def get_urls_from_file(keyword, filename=FILENAME):
                 return urls.split(", ")
     return None, None, None, None
 
-async def scrape_trustradius_product_names_async(base_url):
+def scrape_trustradius_product_names(base_url):
     all_products = []
-    tasks = [scrape_trustradius_page(base_url, i) for i in range(0, 251, 25)]
-    results = await asyncio.gather(*tasks)
-    for products in results:
-        all_products.extend(products)
+    for i in range(0, 251, 25):
+        url = f"{base_url}?f={i}"
+        content = make_request(url)
+        if content:
+            pattern = r'{"@context":"https://schema.org","@type":"ListItem","@id":"https://www.trustradius.com/products/[^"]+/reviews","name":"([^"]+)"'
+            matches = re.findall(pattern, content)
+            all_products.extend(matches)
     return all_products
 
-async def scrape_trustradius_page(base_url, i):
-    url = f"{base_url}?f={i}"
-    content = await make_request_async(url)
-    if content:
-        pattern = r'{"@context":"https://schema.org","@type":"ListItem","@id":"https://www.trustradius.com/products/[^"]+/reviews","name":"([^"]+)"'
-        matches = re.findall(pattern, content)
-        return matches
-    return []
+def scrape_gartner_product_names(url):
+    content = make_request(url)
+    if not content:
+        return []
+    pattern = r'{"id":"[^"]+","seqId":\d+,"name":"([^"]+)","seoName":"[^"]+","vendors":\['
+    matches = re.findall(pattern, content)
+    return matches
 
-async def scrape_gartner_product_names_async(url):
-    content = await make_request_async(url)
-    if content:
-        pattern = r'{"id":"[^"]+","seqId":\d+,"name":"([^"]+)","seoName":"[^"]+","vendors":\['
-        matches = re.findall(pattern, content)
-        return matches
-    return []
+def scrape_capterra_product_names(url):
+    content = make_request(url)
+    if not content:
+        return []
+    pattern = r'{"campaignID":"[^"]+","id":"[^"]+","rating":\d+\.\d+,"slug":"([^"]+)","totalReviews":\d+}'
+    matches = re.findall(pattern, content)
+    return matches
 
-async def scrape_capterra_product_names_async(url):
-    content = await make_request_async(url)
-    if content:
-        pattern = r'{"campaignID":"[^"]+","id":"[^"]+","rating":\d+\.\d+,"slug":"([^"]+)","totalReviews":\d+}'
-        matches = re.findall(pattern, content)
-        return matches
-    return []
-
-async def scrape_github_product_names_async(url):
-    content = await make_request_async(url)
-    if content:
-        pattern = r'style="max-height:275px" href="(/[^/]+/[^/]+)" data-view-component="true"'
-        matches = re.findall(pattern, content)
-        repo_names = [match.split('/')[-1] for match in matches]
-        return repo_names
-    return []
+def scrape_github_product_names(url):
+    content = make_request(url)
+    if not content:
+        return []
+    pattern = r'style="max-height:275px" href="(/[^/]+/[^/]+)" data-view-component="true"'
+    matches = re.findall(pattern, content)
+    repo_names = [match.split('/')[-1] for match in matches]
+    return repo_names
 
 def generate_cpe(product_name):
     normalized_name = product_name.lower().replace(" ", "_").replace(",", "").replace(".", "")
@@ -106,8 +98,7 @@ def save_to_csv(keyword, data, filename="results.csv"):
             cpe = generate_cpe(product)
             writer.writerow([keyword, product, cpe])
 
-async def main(keyword):
-    # Try loading from cache first
+def main(keyword):
     cached_data = load_from_cache(keyword)
     if cached_data:
         logging.info(f"Loaded data for {keyword} from cache.")
@@ -117,31 +108,32 @@ async def main(keyword):
         all_products = []
         
         if trustradius_url:
-            trustradius_products = await scrape_trustradius_product_names_async(trustradius_url)
+            trustradius_products = scrape_trustradius_product_names(trustradius_url)
             all_products.extend(trustradius_products)
             logging.info(f"TrustRadius Products for {keyword}: {', '.join(trustradius_products)}")
 
         if gartner_url:
-            gartner_products = await scrape_gartner_product_names_async(gartner_url)
+            gartner_products = scrape_gartner_product_names(gartner_url)
             all_products.extend(gartner_products)
-            logging.info(f"Gartner Products for {keyword}: {', '.join(gartner_products)}")
 
         if capterra_url:
-            capterra_products = await scrape_capterra_product_names_async(capterra_url)
+            capterra_products = scrape_capterra_product_names(capterra_url)
             all_products.extend(capterra_products)
-            logging.info(f"Capterra Products for {keyword}: {', '.join(capterra_products)}")
 
         if github_url:
-            github_repos = await scrape_github_product_names_async(github_url)
+            github_repos = scrape_github_product_names(github_url)
             all_products.extend(github_repos)
-            logging.info(f"GitHub Repositories for {keyword}: {', '.join(github_repos)}")
 
         save_to_cache(keyword, all_products)
 
+    # Save results to CSV
     save_to_csv(keyword, all_products)
+
+    cpes = [generate_cpe(product) for product in all_products]
+    logging.info(f"\nGenerated CPEs: {', '.join(cpes)}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         logging.error("Usage: main_script.py <keyword>")
     else:
-        asyncio.run(main(sys.argv[1]))
+        main(sys.argv[1])
