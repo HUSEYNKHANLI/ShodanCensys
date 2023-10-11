6900ade28393276e0b17dbb15face664505a15fd
HUSEYNKHANLI <67484695+HUSEYNKHANLI@users.noreply.github.com>
2023-10-11 10:59:27 +0400

    Update sss.py

--- sss.py
+++ sss.py
@@ -1,136 +1,41 @@
-import requests
-import re
-import sys
-import logging
-import pickle
-import os
-import csv
+# ... [rest of the imports and functions remain unchanged]
 
-# Configuration
-FILENAME = "urls.txt"
-CACHE_DIR = "cache"
-
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-
-def get_cache_filename(keyword):
-    return os.path.join(CACHE_DIR, f"{keyword}.pkl")
-
-def load_from_cache(keyword):
-    cache_file = get_cache_filename(keyword)
-    if os.path.exists(cache_file):
-        with open(cache_file, 'rb') as f:
-            return pickle.load(f)
-    return None
-
-def save_to_cache(keyword, data):
-    if not os.path.exists(CACHE_DIR):
-        os.makedirs(CACHE_DIR)
-    cache_file = get_cache_filename(keyword)
-    with open(cache_file, 'wb') as f:
-        pickle.dump(data, f)
-
-def make_request(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.text
-    except requests.RequestException as e:
-        logging.error(f"Failed to retrieve webpage {url}. Error: {e}")
-        return None
-
-def get_urls_from_file(keyword, filename=FILENAME):
-    with open(filename, 'r') as file:
-        for line in file:
-            key, urls = line.strip().split(": ")
-            if key == keyword:
-                return urls.split(", ")
-    return None, None, None, None
-
-def scrape_trustradius_product_names(base_url):
+def scrape_trustradius_product_names(base_url, keyword):
     all_products = []
-    for i in range(0, 251, 25):
-        url = f"{base_url}?f={i}"
+    page = 0
+    while True:
+        if keyword == "firewalls":
+            url = f"{base_url}?f={page*25}#products"
+        else:
+            url = f"{base_url}?f={page*25}"
+        
         content = make_request(url)
         if content:
             pattern = r'{"@context":"https://schema.org","@type":"ListItem","@id":"https://www.trustradius.com/products/[^"]+/reviews","name":"([^"]+)"'
             matches = re.findall(pattern, content)
+            if not matches:  # If no matches are found, we've reached the last page
+                break
             all_products.extend(matches)
+            page += 1
     return all_products
 
-def scrape_gartner_product_names(url):
-    content = make_request(url)
-    if not content:
-        return []
-    pattern = r'{"id":"[^"]+","seqId":\d+,"name":"([^"]+)","seoName":"[^"]+","vendors":\['
-    matches = re.findall(pattern, content)
-    return matches
-
-def scrape_capterra_product_names(url):
-    content = make_request(url)
-    if not content:
-        return []
-    pattern = r'{"campaignID":"[^"]+","id":"[^"]+","rating":\d+\.\d+,"slug":"([^"]+)","totalReviews":\d+}'
-    matches = re.findall(pattern, content)
-    return matches
-
-def scrape_github_product_names(url):
-    content = make_request(url)
-    if not content:
-        return []
-    pattern = r'style="max-height:275px" href="(/[^/]+/[^/]+)" data-view-component="true"'
-    matches = re.findall(pattern, content)
-    repo_names = [match.split('/')[-1] for match in matches]
-    return repo_names
-
-def generate_cpe(product_name):
-    normalized_name = product_name.lower().replace(" ", "_").replace(",", "").replace(".", "")
-    return f"cpe:/a:{normalized_name}:{normalized_name}:::::"
-
-def save_to_csv(keyword, data, filename="results.csv"):
-    """Save the scraped results to a CSV file."""
-    with open(filename, mode='a', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        # Write header only if the file is new
-        if file.tell() == 0:
-            writer.writerow(["Keyword", "Product Name", "CPE"])
-        for product in data:
-            cpe = generate_cpe(product)
-            writer.writerow([keyword, product, cpe])
+# ... [rest of the functions remain unchanged]
 
 def main(keyword):
-    cached_data = load_from_cache(keyword)
-    if cached_data:
-        logging.info(f"Loaded data for {keyword} from cache.")
-        all_products = cached_data
-    else:
-        trustradius_url, gartner_url, capterra_url, github_url = get_urls_from_file(keyword)
-        all_products = []
-        
-        if trustradius_url:
-            trustradius_products = scrape_trustradius_product_names(trustradius_url)
-            all_products.extend(trustradius_products)
-            logging.info(f"TrustRadius Products for {keyword}: {', '.join(trustradius_products)}")
+    # Check if results.csv exists, if not, create it with headers
+    if not os.path.exists("results.csv"):
+        with open("results.csv", mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Keyword", "Product Name", "CPE"])
 
-        if gartner_url:
-            gartner_products = scrape_gartner_product_names(gartner_url)
-            all_products.extend(gartner_products)
+    # ... [rest of the main function remains unchanged, except for the following change]
 
-        if capterra_url:
-            capterra_products = scrape_capterra_product_names(capterra_url)
-            all_products.extend(capterra_products)
+    if trustradius_url:
+        trustradius_products = scrape_trustradius_product_names(trustradius_url, keyword)
+        all_products.extend(trustradius_products)
+        logging.info(f"TrustRadius Products for {keyword}: {', '.join(trustradius_products)}")
 
-        if github_url:
-            github_repos = scrape_github_product_names(github_url)
-            all_products.extend(github_repos)
-
-        save_to_cache(keyword, all_products)
-
-    # Save results to CSV
-    save_to_csv(keyword, all_products)
-
-    cpes = [generate_cpe(product) for product in all_products]
-    logging.info(f"\nGenerated CPEs: {', '.join(cpes)}")
+    # ... [rest of the main function remains unchanged]
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
