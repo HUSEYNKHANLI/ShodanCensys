da45513f83708b51d25efce90c3865c8dec8733e
HUSEYNKHANLI <67484695+HUSEYNKHANLI@users.noreply.github.com>
2023-10-11 13:50:27 +0400

    Update sss.py

--- sss.py
+++ sss.py
@@ -3,6 +3,48 @@ def get_urls_from_file(keyword, filename=FILENAME):
         for line in file:
             key, urls = line.strip().split(": ")
             if key == keyword:
-                urls_list = urls.split(", ")
-                return urls_list[0], urls_list[1], urls_list[2], urls_list[3]
-    return None, None, None, None
+                return urls.split(", ")
+    return []
+
+# ... [rest of the code remains unchanged]
+
+def main(keyword):
+    # ... [rest of the code remains unchanged]
+
+    try:
+        cached_data = load_from_cache(keyword)
+        if cached_data:
+            logging.info(f"Loaded data for {keyword} from cache.")
+            all_products = cached_data
+        else:
+            urls_list = get_urls_from_file(keyword)
+            all_products = []
+            
+            for url in urls_list:
+                if "trustradius" in url:
+                    trustradius_products = scrape_trustradius_product_names(url)
+                    all_products.extend(trustradius_products)
+                    logging.info(f"TrustRadius Products for {keyword}: {', '.join(trustradius_products)}")
+                elif "gartner" in url:
+                    gartner_products = scrape_gartner_product_names(url)
+                    all_products.extend(gartner_products)
+                elif "capterra" in url:
+                    capterra_products = scrape_capterra_product_names(url)
+                    all_products.extend(capterra_products)
+                elif "github" in url:
+                    github_repos = scrape_github_product_names(url)
+                    all_products.extend(github_repos)
+
+            save_to_cache(keyword, all_products)
+
+        # ... [rest of the code remains unchanged]
+
+    except KeyboardInterrupt:
+        logging.info("Interrupted by user. Exiting gracefully.")
+        sys.exit(0)   
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        logging.error("Usage: main_script.py <keyword>")
+    else:
+        main(sys.argv[1])
