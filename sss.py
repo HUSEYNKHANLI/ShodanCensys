daacf896dac858566309726143fbbed816907bbb
HUSEYNKHANLI <67484695+HUSEYNKHANLI@users.noreply.github.com>
2023-10-11 12:02:13 +0400

    Update sss.py

--- sss.py
+++ sss.py
@@ -1,11 +1,19 @@
-def make_request(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
-    try:
-        response = requests.get(url, headers=headers, verify=False)  # Added headers and verify=False
-        response.raise_for_status()
-        return response.text
-    except requests.RequestException as e:
-        logging.error(f"Failed to retrieve webpage {url}. Error: {e}")
-        return None
+def scrape_trustradius_product_names(base_url, keyword):
+    all_products = []
+    i = 0
+    while True:
+        if keyword == "firewalls":
+            url = f"{base_url}?f={i}#products"
+        else:
+            url = f"{base_url}?f={i}"
+        content = make_request(url)
+        if content:
+            pattern = r'{"@context":"https://schema.org","@type":"ListItem","@id":"https://www.trustradius.com/products/[^"]+/reviews","name":"([^"]+)"'
+            matches = re.findall(pattern, content)
+            if not matches:  # No product names found, indicating it's the last page
+                break
+            all_products.extend(matches)
+            i += 25
+        else:
+            break
+    return all_products
